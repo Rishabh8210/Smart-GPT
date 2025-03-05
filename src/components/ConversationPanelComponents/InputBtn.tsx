3c63734ea545968d0b42f9edce61ce137c592403
Rishabh Pandey <rishabhpandey8092@gmail.com>
2025-03-05 22:07:31 +0530

    Fix: Fixed the chat width issue and empty input field issue

--- src/components/ConversationPanelComponents/InputBtn.tsx
+++ src/components/ConversationPanelComponents/InputBtn.tsx
@@ -13,10 +13,12 @@ export const InputBtn = ({setChats}:IChatInput) => {
         onChange={(e) => setSearchText(e.target.value)}
         value={searchText}
       />
-      <button className=" absolute right-2 bottom-2 h-fit w-fit p-1.5 bg-white rounded-full cursor-pointer" onClick={() => {
-          setChats((prev:any) => [...prev, {name:'user', content:searchText}])
-          setChats((prev:any) => [...prev, {name:'gpt', content:'Js is a widely used programming language which is use to manipulte websites, pages'}])
-          setSearchText('');
+      <button className=" absolute right-2 bottom-2 h-fit w-fit p-1.5 bg-white hover:bg-zinc-400 rounded-full cursor-pointer" onClick={() => {
+          if(searchText.length != 0){
+            setChats((prev:any) => [...prev, {name:'user', content:searchText}])
+            setChats((prev:any) => [...prev, {name:'gpt', content:'Js is a widely used programming language which is use to manipulte websites, pages'}])
+            setSearchText('');
+          }
         }}>
         <img className="w-6" src="search.svg" />
       </button>
--- src/components/ConversationPanelComponents/MainContainer.tsx
+++ src/components/ConversationPanelComponents/MainContainer.tsx
@@ -3,7 +3,6 @@ import { InputBtn } from "./InputBtn"
 
 export const MainContainer = () => {
   const [chats, setchats] = useState<any[]>([]);
-
   if (chats.length == 0) {
     return (
       <div className="h-[80%] w-full flex flex-col justify-center items-center gap-4 overflow-hidden">
@@ -15,14 +14,14 @@ export const MainContainer = () => {
 
   return (
     <div className="h-full w-full flex flex-col justify-center items-center overflow-hidden">
-      <div className="h-[80%] p-4 w-full flex flex-col items-center text-white gap-4 overflow-auto">
+      <div  className="h-[80%] p-4 w-full flex flex-col items-center text-white gap-4 overflow-auto">
         {
           chats.length != 0 && chats.map((chat, ind) => {
             if (ind % 2) {
               return (
-                <div className="h-fit w-[50vw] flex justify-start">
+                <div className="h-fit max-w-[50vw] w-[50vw] flex justify-start">
                   <div className="h-fit w-full flex px-4 py-2 ">
-                    <p>{chat.content}</p>
+                    <p className="w-full break-words">{chat.content}</p>
                   </div>
                 </div>
 
@@ -30,8 +29,8 @@ export const MainContainer = () => {
             } else {
               return (
                 <div className="h-fit w-[50vw] flex justify-end">
-                  <div className="h-fit w-[60%] flex px-4 py-2 bg-zinc-700 rounded-2xl">
-                    <p className="">{chat.content}</p>
+                  <div className="h-fit w-fit max-w-[60%] flex px-4 py-2 bg-zinc-700 rounded-2xl">
+                    <p className="w-full break-words">{chat.content}</p>
                   </div>
                 </div>
               )
